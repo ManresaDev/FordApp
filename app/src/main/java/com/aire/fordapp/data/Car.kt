16aa40977641e8d92eb6cf73e67d32e2a4e2b063
manuel.manresa <manuel.manresa@airenetworks.es>
2024-12-17 18:14:32 +0100

    Añadido lista

---
+++ app/src/main/java/com/aire/fordapp/data/Car.kt
@@ -0,0 +1,13 @@
+package com.aire.fordapp.data
+
+data class Car(
+    val name : String
+)
+
+val cars = mutableListOf(
+    Car("Puma"),
+    Car("Focus"),
+    Car("Mustang"),
+    Car("Fiesta"),
+    Car("Mondeo")
+)
--- app/src/main/java/com/aire/fordapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/aire/fordapp/ui/screens/HomeScreen.kt
@@ -2,14 +2,22 @@ package com.aire.fordapp.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.aire.fordapp.data.cars
+
 
 @Composable
 fun HomeScreen(){
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-        Text(text = "Home")
+        LazyColumn {
+            items(cars){ car ->
+                Text(text = car.name)
+            }
+        }
     }
 }
